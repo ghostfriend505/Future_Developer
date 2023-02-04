6389bb2a13a58f14ed4e2d3037712798bc162828
Ujjwal kumar <113157397+ghostfriend505@users.noreply.github.com>
2023-02-04 08:11:02 +0530

    Blockletters..cpp
    This is how you can print BLOCK LETTERS in C++ when you don't know anything about if/else statement .

---
+++ Blockletter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+    std::cout << "HELLO WORLD\n";
+    std::cout << "H E L L O     W O R L D   A G A I N\n";
+    std::cout << "O O O O O\n";
+    std::cout << "O  O  O O\n";
+    std::cout << "O       O\n";
+    std::cout << "O   O   O\n";
+    std::cout << "O   O   O\n";
+    std::cout << "O   O   O\n";
+    std::cout << "O       O\n";
+    std::cout << "O       O\n";
+    std::cout << "O  O  O O\n";
+}
